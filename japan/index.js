5a75684eb2228588cce652a2e148c7e0bd417688
tristen <tristen.brown@gmail.com>
2020-04-03 15:30:43 -0400

    Some structural changes

---
+++ japan/index.js
@@ -0,0 +1,15 @@
+const buildings = require('./buildings.json');
+const landAndWater = require('./land-and-water.json');
+const naturalLabels = require('./natural-labels.json');
+const pointsOfInterest = require('./points-of-interest.json');
+const roadNetwork = require('./road-network.json');
+const transit = require('./transit.json');
+
+module.exports = {
+  buildings,
+  landAndWater,
+  naturalLabels,
+  pointsOfInterest,
+  roadNetwork,
+  transit
+};
--- worldwide/index.js
+++ worldwide/index.js
@@ -1,17 +1,27 @@
+const adminBooundaries = require('./admin-boundaries.json');
+const buildings = require('./buildings.json');
 const highwayShields = require('./highway-shields.json');
+const landAndWater = require('./land-and-water.json');
 const naturalLabels = require('./natural-labels.json');
 const pointsOfInterest = require('./points-of-interest.json');
 const roadNetwork = require('./road-network.json');
-const settlements = require('./settlements.json');
 const settlementSubdivisions = require('./settlement-subdivisions.json');
+const settlements = require('./settlements.json');
+const terrain = require('./terrain.json');
 const transit = require('./transit.json');
+const walkingCycling = require('./walking-cycling.json');
 
 module.exports = {
+  adminBooundaries,
+  buildings,
   highwayShields,
+  landAndWater,
   naturalLabels,
   pointsOfInterest,
   roadNetwork,
-  settlements,
   settlementSubdivisions,
-  transit
+  settlements,
+  terrain,
+  transit,
+  walkingCycling
 };
